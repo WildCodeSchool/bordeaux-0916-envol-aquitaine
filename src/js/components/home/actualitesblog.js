532f27c2a07d2362cc84e5cee0935a87fdca2105
Audrey <alary.audrey4712@gmail.com>
2016-10-07 12:02:35 +0200

    ajout V1

---
+++ src/js/components/home/actualitesblog.js
@@ -0,0 +1,6 @@
+((app) => {
+    'use strict'
+    app.component("actualitesblog", {
+        templateUrl: 'js/components/home/actualitesblog.html'
+    })
+})(angular.module('app.common'))
---
+++ src/js/components/home/actualitesfb.js
@@ -0,0 +1,6 @@
+((app) => {
+    'use strict'
+    app.component("actualitesfb", {
+        templateUrl: 'js/components/home/actualitesfb.html'
+    })
+})(angular.module('app.common'))
