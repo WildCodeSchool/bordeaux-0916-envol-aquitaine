850805a3a39093df208b0c95bd246573ebe13c7a
apprenti <nans.mourgues@hotmail.fr>
2016-10-18 14:36:39 +0200

    Mon blog de ouf

--- src/js/components/blog/blog.js
+++ src/js/components/blog/blog.js
@@ -1,6 +1,31 @@
 ((app) => {
     'use strict'
     app.component("blog", {
-        templateUrl: 'js/components/blog/blog.html'
+        templateUrl: 'js/components/blog/blog.html',
+        // mon coposant sera affiché dans le fichier blog.html//
+        controller: ['$http', function($http) {
+          //mon cotroller utilisera le service http qui est ://
+            angular.extend(this, {
+                //  this est mon controlleur //
+                posts:[],
+                post: {},
+                //mes posts seront dans un tableau//
+                $onInit() {
+                  //Aprés le chargement du component faire ce qui suit//
+                    this.posts = [
+                      {title:"Whaouuuuuuu", author:"Alexis", createAt:"12/00/2015", body:"dksjfhkdjshfkjsdhfkdshfkjsdhfkjshdfkshdkfhdskfh", image:"http://loremflickr.com/600/300"},
+                      {title:"Azertyyyyyyyyyyyyyy", author:"Oussou", createAt:"12/00/2015", body:"dksjfhkdjshfkjsdhfkdshfkjsdhfkjshdfkshdkfhdskfh", image:"http://loremflickr.com/600/300"}
+                    ]
+                    //Declaration des données de 2 posts//
+                },
+                addPost(){
+                  this.post.createdOn = Date.now();
+                  this.posts.unshift(this.post);
+                  this.tab = 0;
+                  this.post ={};
+                }
+            })
+        }]
+
     })
 })(angular.module('app.blog'))
