9f193b6d705413129fe68f5833f3890f34ff43ec
Audrey <alary.audrey4712@gmail.com>
2016-10-24 17:51:51 +0200

    inscription

---
+++ src/js/components/inscription-tarif/inscription.js
@@ -0,0 +1,12 @@
+((app) => {
+    'use strict'
+    app.component("inscription", {
+        templateUrl: 'js/components/inscription-tarif/inscription.html',
+        controller: [function(){
+          angular.extend(this, {
+
+          })
+        }]
+    })
+
+  })(angular.module('app.inscription'))
