d84a89c66177d9c98ddc292bedd933e78f648260
oussou-dev <oussou.dev@gmail.com>
2016-10-26 14:04:51 +0200

    Add jbe/review

--- src/js/components/blog/blog.js
+++ src/js/components/blog/blog.js
@@ -4,25 +4,51 @@
         templateUrl: 'js/components/blog/blog.html',
         // mon coposant sera affiché dans le fichier blog.html//
         controller: ['$http', function($http) {
-          //mon cotroller utilisera le service http qui est ://
+            //mon cotroller utilisera le service http qui est ://
             angular.extend(this, {
                 //  this est mon controlleur //
-                posts:[],
-                post: {},
+                posts: [],
                 //mes posts seront dans un tableau//
                 $onInit() {
-                  //Aprés le chargement du component faire ce qui suit//
-                    this.posts = [
-                      {title:"Whaouuuuuuu", author:"Alexis", createdAt:"12/00/2015", body:"dksjfhkdjshfkjsdhfkdshfkjsdhfkjshdfkshdkfhdskfh", image:"http://loremflickr.com/600/300"},
-                      {title:"Azertyyyyyyyyyyyyyy", author:"Oussou", createdAt:"12/00/2015", body:"dksjfhkdjshfkjsdhfkdshfkjsdhfkjshdfkshdkfhdskfh", image:"http://loremflickr.com/600/300"}
-                    ]
-                    //Declaration des données de 2 posts//
+                    //Aprés le chargement du component faire ce qui suit//
+                    this.posts = [{
+                            title: "Whaouuuuuuu",
+                            author: "Alexis",
+                            createdAt: "1477409698997",
+                            body: "dksjfhkdjshfkjsdhfkdshfkjsdhfkjshdfkshdkfhdskfh",
+                            image: "http://loremflickr.com/600/300"
+                        }, {
+                            title: "Azertyyyyyyyyyyyyyy",
+                            author: "Oussou",
+                            createdAt: "1477409698989",
+                            body: "dksjfhkdjshfkjsdhfkdshfkjsdhfkjshdfkshdkfhdskfh",
+                            image: "http://loremflickr.com/600/300"
+                        },
+                        {
+                            title: "Whauuuu",
+                            author: "Oussou",
+                            createdAt: "1477409698921",
+                            body: "Lorem",
+                            image: "http://loremflickr.com/600/300"
+                        }]
+                        //Declaration des données de 2 posts//
                 },
-                addPost(){
-                  this.post.createdAt = Date.now();
-                  this.posts.unshift(this.post);
-                  this.tab = 0;
-                  this.post ={};
+                savePost() {
+                  this.edit = false;
+                    if (!this.postSelected.createdAt) {
+                        //NOUVEAU
+                        this.postSelected.createdAt = Date.now();
+                        this.posts.unshift(this.postSelected);
+                        this.postSelected = null;
+                    } else {
+                        //EDITION
+                        this.posts.forEach((post) => {
+                          if (post.createdAt === this.postSelected.createdAt){
+                            post = this.postSelected
+                            return
+                          }
+                        })
+                    }
                 }
             })
         }]
