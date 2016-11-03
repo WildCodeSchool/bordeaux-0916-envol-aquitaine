76d5b860bcd102c477d65af973f782d52dfc157e
Audrey <alary.audrey4712@gmail.com>
2016-11-03 17:06:11 +0100

    edit

--- src/js/components/home/carousel.js
+++ src/js/components/home/carousel.js
@@ -1,6 +1,14 @@
 ((app) => {
     'use strict'
     app.component("carousel", {
-        templateUrl: 'js/components/home/carousel.html'
+        templateUrl: 'js/components/home/carousel.html',
+        controller: [function() {
+            angular.extend(this, {
+                page: {
+                    name: 'carousel',
+                    content: {}
+                }
+            })
+        }]
     })
 })(angular.module('app.home'))
