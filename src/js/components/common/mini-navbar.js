7390306fb8434db1f197e7621b801b5dba07c111
jbehuet <jbehuet@gmail.com>
2016-11-03 10:45:08 +0100

    Add contenteditable

--- src/js/components/common/mini-navbar.js
+++ src/js/components/common/mini-navbar.js
@@ -8,31 +8,31 @@
         templateUrl: 'js/components/common/mini-navbar.html',
         controller: function(PageService, UserService) {
             angular.extend(this, {
-                // initialData: null,
-                // $onInit() {
-                //   UserService.getCurrent().then((user) => {
-                //         this.user = user
-                //     }).catch((err) => {
-                //
-                //     })
-                //
-                //     PageService.get(this.ngModel.name).then((res) => {
-                //         if (res.data.content)
-                //             res.data.content = JSON.parse(res.data.content)
-                //         this.ngModel = res.data
-                //
-                //         this.initialData = this.ngModel
-                //     })
-                // },
-                // save() {
-                //     PageService.save(Object.assign({}, this.ngModel)).then((res) => {
-                //         this.editMode = false
-                //     })
-                // },
-                // cancel() {
-                //     this.editMode = false
-                //     this.ngModel = this.initialData
-                // }
+                initialData: null,
+                $onInit() {
+                  UserService.getCurrent().then((user) => {
+                        this.user = user
+                    }).catch((err) => {
+
+                    })
+
+                    PageService.get(this.ngModel.name).then((res) => {
+                        if (res.data.content)
+                            res.data.content = JSON.parse(res.data.content)
+                        this.ngModel = res.data
+
+                        this.initialData = this.ngModel
+                    })
+                },
+                save() {
+                    PageService.save(Object.assign({}, this.ngModel)).then((res) => {
+                        this.editMode = false
+                    })
+                },
+                cancel() {
+                    this.editMode = false
+                    this.ngModel = this.initialData
+                }
             })
         }
     })
--- src/js/components/inscription-tarif/inscription.js
+++ src/js/components/inscription-tarif/inscription.js
@@ -2,11 +2,14 @@
     'use strict'
     app.component("inscription", {
         templateUrl: 'js/components/inscription-tarif/inscription.html',
-        controller: [function(){
-          angular.extend(this, {
-
-          })
+        controller: [function() {
+            angular.extend(this, {
+                page: {
+                    name: 'inscription',
+                    content: {}
+                }
+            })
         }]
     })
 
-  })(angular.module('app.inscription'))
+})(angular.module('app.inscription'))
