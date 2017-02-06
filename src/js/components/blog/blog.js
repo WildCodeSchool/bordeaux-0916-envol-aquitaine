3519d8a9b682f367ca6497348b5632138f06922e
jbehuet <jbehuet@gmail.com>
2017-02-06 14:16:22 +0100

    Edit blog

--- src/js/components/blog/blog.js
+++ src/js/components/blog/blog.js
@@ -3,20 +3,62 @@
     app.component("blog", {
         templateUrl: 'js/components/blog/blog.html',
         // mon coposant sera affiché dans le fichier blog.html//
-        controller: ['$http', 'PostService', function($http, PostService) {
+        controller: ['$http', 'PostService', 'UserService', function($http, PostService, UserService) {
             //mon cotroller utilisera le service http qui est ://
             angular.extend(this, {
                 posts: [],
-            $onInit() {
-                this.startIndex = 0
-                PostService.getPublished().then((res) => {
-                    this.posts = res.data;
-                    // this.posts.body = res.data[1].content
-                    console.log(res.data[1].content)
-                  })
-            }
-        })
-    }]
+                $onInit() {
+                    this.edit = false
+                    PostService.getPublished().then((res) => {
+                        this.posts = res.data;
+                    })
 
-})
+                    UserService.getCurrent().then((user) => {
+                        this.isAdmin = user.isAdmin
+                    }).catch((err) => {
+
+                    })
+                },
+                edit(post) {
+                    this.selectedPost = post
+                },
+                add() {
+                    this.selectedPost = {
+                        isDraft: true
+                    }
+                    this.edit = true
+                },
+                save() {
+                    PostService.save(this.selectedPost).then((res) => {
+                        if (angular.isUndefined(this.selectedPost._id))
+                            this.posts[this.posts.length - 1] = res.data
+                        toastr.success(`${this.selectedPost.title} saved`)
+                    }).catch((err) => {
+                        toastr.error(`${err.data}`)
+                    })
+
+                },
+                delete(post) {
+                    let idx = this.posts.find((el, idx) => {
+                        if (el._id === post._id)
+                            return idx
+                    })
+                    debugger
+                    // this.posts.splice(idx, 1)
+                    // if (angular.isDefined(post._id)) {
+                    //     PostService.delete(post).then(() => {
+                    //         toastr.success(`${post.title} deleted`)
+                    //         this.selectedPost = null
+                    //     }).catch((err) => {
+                    //         toastr.error(`${err.data}`)
+                    //     })
+                    // } else {
+                    //     this.selectedPost = null
+                    // }
+
+                }
+            })
+        }]
+
+    })
 })(angular.module('app.blog'))
--- src/js/components/home/actualitesblog.js
+++ src/js/components/home/actualitesblog.js
@@ -1,6 +1,11 @@
 ((app) => {
     'use strict'
     app.component("actualitesblog", {
-        templateUrl: 'js/components/home/actualitesblog.html'
+        templateUrl: 'js/components/home/actualitesblog.html',
+        controller: function(PostService){
+          PostService.getPublished().then((res) => {
+              this.posts = res.data;
+          })
+        }
     })
 })(angular.module('app.common'))
