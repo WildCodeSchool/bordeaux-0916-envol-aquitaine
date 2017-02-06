0522b7de81af0ccb36aaca90a2db3fc8a3501d1c
jbehuet <jbehuet@gmail.com>
2017-02-06 14:27:55 +0100

    Fix inject error

--- src/js/components/home/actualitesblog.js
+++ src/js/components/home/actualitesblog.js
@@ -2,10 +2,10 @@
     'use strict'
     app.component("actualitesblog", {
         templateUrl: 'js/components/home/actualitesblog.html',
-        controller: function(PostService){
-          PostService.getPublished().then((res) => {
-              this.posts = res.data;
-          })
-        }
+        controller: ['PostService', function(PostService) {
+            PostService.getPublished().then((res) => {
+                this.posts = res.data;
+            })
+        }]
     })
 })(angular.module('app.common'))
